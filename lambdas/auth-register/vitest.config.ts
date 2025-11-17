abba5d49ecee9ba3d348f6783a369dd2dbe1d65a
Carlos Tolentino <carlost@gopassi.com>
2025-11-17 15:25:13 -0400

    add port/adapter console.log

--- lambdas/auth-register/vitest.config.ts
+++ lambdas/auth-register/vitest.config.ts
@@ -4,10 +4,16 @@ import { defineConfig } from 'vitest/config';
 // @ts-ignore
 const sharedDir = fileURLToPath(new URL('../shared', import.meta.url));
 
+const sharedSetupFile = fileURLToPath(
+    // @ts-ignore
+  new URL('../shared/tests/setup/mock-console.ts', import.meta.url)
+);
+
 export default defineConfig({
   test: {
     environment: 'node',
     include: ['src/**/*.test.ts', '../shared/**/*.test.ts'],
+    setupFiles: [sharedSetupFile],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
